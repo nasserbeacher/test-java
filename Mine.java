4c6f858919b7e1d6eb25baed5e32db913212fd6c
Nasser beacher <nasser.beacher@amalitechtraining.org>
2024-08-27 11:09:53 +0000

    mine  java application

---
+++ Mine.java
@@ -0,0 +1,50 @@
+package mine;
+
+public class Mine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("Hello AmaliTech");
+    
+    String him = "What i have been able to acomplish so far";
+    System.out.println(him);
+    
+    int y = 76;
+    int x = 90;
+    int z = 62;
+    int S = y+x+z;
+    int addition = S;
+    double D = y/z;
+    double division = D;
+    System.out.println("The sum of all is " + addition);
+    System.out.println("The division of 76 and 62 is " + division);
+    
+    int v = 35;
+    int w = 32;
+    double l = 14.4;
+    if (v > w) {
+    System.out.println("Positive");
+    }
+    else {
+    	System.out.println("Negative");
+    }
+    
+    double m = 3.5;
+    double n = 4.5;
+    double t = 1.2;
+    if (m == n) {
+    	System.out.println("3.5 is equal to 4.5");
+    }
+    else if (m > n) {
+    	System.out.println("3.5 is greater than 4.5");
+    }
+    else {
+    	System.out.println("3.5 is not greater than 4.5");
+    }
+    
+    }
+    
+   
+		
+	}
+
